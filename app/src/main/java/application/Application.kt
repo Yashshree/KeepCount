0453a30d35d5408a33a5ac3f7d5eb62a8aaf05c6
yashshree.chavan <yashshree29.chavan@gmail.com>
2019-06-04 18:56:23 +0530

    google sign in

---
+++ app/src/main/java/application/Application.kt
@@ -0,0 +1,13 @@
+package application
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+
+    }
+}
--- app/src/main/java/com/app/keepcount/MainActivity.kt
+++ app/src/main/java/com/app/keepcount/MainActivity.kt
@@ -2,9 +2,9 @@ package com.app.keepcount
 
 import android.app.Dialog
 import android.content.DialogInterface
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
