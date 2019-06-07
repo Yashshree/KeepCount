3d06615b8e485fb5e0813b72c652fab65e491bbb
yashshree.chavan <yashshree29.chavan@gmail.com>
2019-06-07 18:24:36 +0530

    google sign in completed

--- app/src/main/java/com/app/keepcount/login/view/LoginActivity.kt
+++ app/src/main/java/com/app/keepcount/login/view/LoginActivity.kt
@@ -17,17 +17,19 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import android.R.attr.data
+import android.media.MediaPlayer
+import android.view.View
 import android.view.animation.AnimationUtils
 import com.google.android.gms.tasks.Task
 import androidx.annotation.NonNull
 import com.google.android.gms.tasks.OnCompleteListener
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
 
 
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
-
-
-
-class LoginActivity : AppCompatActivity() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
@@ -36,9 +38,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
-      val  androidZoomOutAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
-        txtAppName.startAnimation(androidZoomOutAnimation);
+        /*   val androidZoomOutAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in_animation)
+           txtAppName.startAnimation(androidZoomOutAnimation)*/
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -52,17 +53,39 @@ class LoginActivity : AppCompatActivity() {
 
         txtAppName.setOnClickListener {
             //FirebaseAuth.getInstance().signOut()
-
             googleSignInClient.signOut()
                 .addOnCompleteListener(this, OnCompleteListener<Void> {
                     Toast.makeText(this@LoginActivity, "logout Sucessfully", Toast.LENGTH_SHORT).show()
                 })
         }
+
+        playCoinSound()
+
+      /*  val anim = RotateAnimation(0f, 350f, 15f, 15f)
+        anim.interpolator = LinearInterpolator()
+        anim.repeatCount = Animation.INFINITE
+        anim.duration = 700
+
+        imgCoin.startAnimation(anim)*/
+    }
+
+    private fun playCoinSound() {
+        val afd = getAssets().openFd("coins.mp3");
+        val player = MediaPlayer()
+        player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength())
+        player.prepare()
+        player.start()
     }
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+
+        }
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -115,9 +138,9 @@ class LoginActivity : AppCompatActivity() {
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
-     //   val currentUser = auth.currentUser
+        //   val currentUser = auth.currentUser
         val account = GoogleSignIn.getLastSignedInAccount(this)
-        Toast.makeText(this, if(account!=null)account.email else "new user", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, if (account != null) account.email else "new user", Toast.LENGTH_SHORT).show()
     }
 
     private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
@@ -137,8 +160,6 @@ class LoginActivity : AppCompatActivity() {
                     Snackbar.make(rootLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
                     //  Toast.makeText(this,currentUser.toString(),Toast.LENGTH_SHORT).show()
                 }
-
-                // ...
             }
     }
 }
