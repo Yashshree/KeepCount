de73b0f52584c8b79aa9db87eef31245a45b1001
yashshree <yashshree29.chavan@gmail.com>
2020-05-26 11:24:21 +0530

    dashboard design started

--- app/src/main/java/com/app/keepcount/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/app/keepcount/dashboard/DashboardActivity.kt
@@ -2,12 +2,25 @@ package com.app.keepcount.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toolbar
 import com.app.keepcount.R
+import kotlinx.android.synthetic.main.toolbar_layout.*
+import kotlinx.android.synthetic.main.toolbar_layout.view.*
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+       setSupportActionBar(toolbarLayout)
+
+
     }
 }
